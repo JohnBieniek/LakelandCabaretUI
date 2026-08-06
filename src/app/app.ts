08058d220de7dd06e4b06f91447340e86bdc243f
John Bieniek <1126076+JohnBieniek@users.noreply.github.com>
2026-08-05 20:34:47 -0400

    Use tight Whoville DJ photo

--- src/app/app.ts
+++ src/app/app.ts
@@ -36,7 +36,10 @@ export class App {
       alt: 'Lakeland Cabaret DJ performing at a mall Christmas event',
     },
     { src: '/images/photography.webp', alt: 'Lakeland Cabaret DJ performing at a holiday event' },
-    { src: '/images/whoville dj.jpg', alt: 'Lakeland Cabaret DJ performing at a Whoville event' },
+    {
+      src: '/images/whoville tight.png',
+      alt: 'Lakeland Cabaret DJ performing at a Whoville event',
+    },
     { src: '/images/dance floor dj.jpg', alt: 'Lakeland Cabaret DJ beside a dance floor' },
     { src: '/images/dj pov.jpg', alt: 'A DJ booth view of a Lakeland Cabaret event' },
     { src: '/images/suit dj.jpg', alt: 'Lakeland Cabaret DJ performing in a suit' },
